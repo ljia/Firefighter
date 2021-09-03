015bb3aa3a45b2f470ec66c74ee888a36f505143
Allen <allen_jia@yahoo.com>
2021-09-03 19:27:19 -0400

    Replaced full qualified class name with import

--- app/src/main/java/main/impls/CityImpl.java
+++ app/src/main/java/main/impls/CityImpl.java
@@ -1,13 +1,14 @@
 package main.impls;
 
 import main.api.Building;
+import main.api.City;
 import main.api.CityNode;
 import main.api.FireDispatch;
 import main.api.exceptions.InvalidDimensionException;
 import main.api.exceptions.OutOfCityBoundsException;
 import main.firefighters.FireDispatchImpl;
 
-public class CityImpl implements main.api.City {
+public class CityImpl implements City {
   private final FireStation fireStation;
   private final Building[][] buildingGrid;
   private FireDispatch fireDispatch;
